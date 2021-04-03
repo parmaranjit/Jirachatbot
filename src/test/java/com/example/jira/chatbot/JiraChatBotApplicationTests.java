6cd3a51958995a26aa26610ff00fe0355aef942a
John Smith <parmaranjit@gmail.com>
2021-04-03 21:48:26 +0530

    Revert "Revert "Jira chat bot source code setup""
    This reverts commit 078296f

---
+++ src/test/java/com/example/jira/chatbot/JiraChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jira.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
