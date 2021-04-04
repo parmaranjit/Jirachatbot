fe471c8989c1a4302fded752807255ec094de30a
John Smith <parmaranjit@gmail.com>
2021-04-04 18:11:18 +0530

    Generate Json response and send back to client

--- src/main/java/com/example/jira/chatbot/JiraChatBotApplication.java
+++ src/main/java/com/example/jira/chatbot/JiraChatBotApplication.java
@@ -4,6 +4,8 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.GetRequest;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -22,6 +24,7 @@ public class JiraChatBotApplication {
     private String userId;
     @Value("${user.token}")
     private String token;
+    private String projectName = "CSSUPINT";
 
     public static void main(String[] args) {
         SpringApplication.run(JiraChatBotApplication.class, args);
@@ -29,22 +32,63 @@ public class JiraChatBotApplication {
 
     @GetMapping("/jira")
     public String getQuestion(@RequestParam(value = "ask", defaultValue = "Work is in progress...") String ask) {
-        HttpResponse<JsonNode> response = null;
+        JSONArray queryResponse = null;
         try {
-            response = Unirest.get(jiraUrl + "/rest/api/3/search")
-                    .basicAuth(userId, token)
-                    .header("Accept", "application/json")
-                    .queryString("jql", "text ~ \"" + ask + "\"")
+            HttpResponse<JsonNode> response = null;
+            response = getConnection()
+                    .queryString("jql", "project = " + projectName + " AND text ~ \"" + ask + "\"")
                     .queryString("maxResults", "5")
-                    .queryString("fields", "key,summary,status")
+                    .queryString("fields", "key,summary,status,versions,fixVersions,labels")
                     .queryString("fieldsByKeys", "true")
                     .asJson();
+
+            queryResponse = fetchFieldsJson(response);
         } catch (UnirestException e) {
             System.out.println("Exception:" + e.getMessage());
         }
-
-        System.out.println(response.getBody());
-        return response.getBody().toString();
+        return String.valueOf(queryResponse);
     }
 
+    private JSONArray fetchFieldsJson(HttpResponse<JsonNode> response) {
+        JSONArray jsonResponse = new JSONArray();
+        JSONArray issues = (JSONArray) (response.getBody().getObject()).get("issues");
+        for (Object itr : issues) {
+            String ticketId;
+            String ticketSummary;
+            String ticketState;
+            StringBuilder ticketCSFixVersion = new StringBuilder();
+            JSONObject jsonObject = new JSONObject();
+
+            JSONObject issue = (JSONObject) itr;
+            ticketId = (String) issue.get("key");
+            JSONObject fields = (JSONObject) issue.get("fields");
+            ticketSummary = (String) fields.get("summary");
+            JSONObject status = (JSONObject) fields.get("status");
+            ticketState = (String) status.get("name");
+            if (ticketState.equalsIgnoreCase("Closed")) {
+                JSONArray fixVersions = (JSONArray) fields.get("fixVersions");
+                for (Object versionObject : fixVersions) {
+                    JSONObject fixVersion = (JSONObject) versionObject;
+                    ticketCSFixVersion.append(fixVersion.get("name")).append(" ");
+                }
+            }
+
+
+            jsonObject.put("id", jiraUrl + "/browse/" + ticketId);
+            jsonObject.put("summary", ticketSummary);
+            jsonObject.put("state", ticketState);
+            if (ticketCSFixVersion.length() > 0) {
+                jsonObject.put("fixVersions", ticketCSFixVersion.toString().trim());
+            }
+            jsonResponse.put(jsonObject);
+        }
+
+        return jsonResponse;
+    }
+
+    private GetRequest getConnection() {
+        return Unirest.get(jiraUrl + "/rest/api/3/search")
+                .basicAuth(userId, token)
+                .header("Accept", "application/json");
+    }
 }
