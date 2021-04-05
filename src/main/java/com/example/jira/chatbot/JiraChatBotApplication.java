103da8546f7397c130083d7bd6abf48a6e99d1b8
Akshay Rokade <akshay.rokade@contentserv.com>
2021-04-05 20:26:16 +0530

    Create Ticket code added.

--- src/main/java/com/example/jira/chatbot/JiraChatBotApplication.java
+++ src/main/java/com/example/jira/chatbot/JiraChatBotApplication.java
@@ -1,7 +1,12 @@
-package com.example.jira.chatbot;
+package com.jira.chatbot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.GetRequest;
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 
 @SpringBootApplication
 @RestController
@@ -24,7 +31,7 @@ public class JiraChatBotApplication {
     private String userId;
     @Value("${user.token}")
     private String token;
-    private String projectName = "CSSUPINT";
+    private String projectName = "H3S";
 
     public static void main(String[] args) {
         SpringApplication.run(JiraChatBotApplication.class, args);
@@ -38,7 +45,7 @@ public class JiraChatBotApplication {
             response = getConnection()
                     .queryString("jql", "project = " + projectName + " AND text ~ \"" + ask + "\"")
                     .queryString("maxResults", "5")
-                    .queryString("fields", "key,summary,status,versions,fixVersions,labels")
+                    .queryString("fields", "key,issuetype,summary,status,versions,fixVersions,labels,project,assignee,priority")
                     .queryString("fieldsByKeys", "true")
                     .asJson();
 
@@ -52,6 +59,7 @@ public class JiraChatBotApplication {
     private JSONArray fetchFieldsJson(HttpResponse<JsonNode> response) {
         JSONArray jsonResponse = new JSONArray();
         JSONArray issues = (JSONArray) (response.getBody().getObject()).get("issues");
+        //System.out.println(issues);
         for (Object itr : issues) {
             String ticketId;
             String ticketSummary;
@@ -65,6 +73,7 @@ public class JiraChatBotApplication {
             ticketSummary = (String) fields.get("summary");
             JSONObject status = (JSONObject) fields.get("status");
             ticketState = (String) status.get("name");
+
             if (ticketState.equalsIgnoreCase("Closed")) {
                 JSONArray fixVersions = (JSONArray) fields.get("fixVersions");
                 for (Object versionObject : fixVersions) {
@@ -84,6 +93,95 @@ public class JiraChatBotApplication {
         }
 
         return jsonResponse;
+    }
+
+    @GetMapping("/create")
+    public void createIssue() throws UnirestException {
+        // The payload definition using the Jackson library
+        JsonNodeFactory jnf = JsonNodeFactory.instance;
+        ObjectNode payload = jnf.objectNode();
+        {
+            ObjectNode update = payload.putObject("update");
+            {
+            }
+            ObjectNode fields = payload.putObject("fields");
+            {
+                fields.put("summary", "Create Ticket Test 1");
+
+                ObjectNode issuetype = fields.putObject("issuetype");
+                {
+                    issuetype.put("id", "1");
+                }
+
+                ObjectNode project = fields.putObject("project");
+                {
+                    project.put("id", "26914");
+                }
+                ObjectNode description = fields.putObject("description");
+                {
+                    description.put("type", "doc");
+                    description.put("version", 1);
+                    ArrayNode content = description.putArray("content");
+                    ObjectNode content0 = content.addObject();
+                    {
+                        content0.put("type", "paragraph");
+                        content = content0.putArray("content");
+                        content0 = content.addObject();
+                        {
+                            content0.put("text", "Order entry fails when selecting supplier.");
+                            content0.put("type", "text");
+                        }
+                    }
+                }
+                ObjectNode reporter = fields.putObject("reporter");
+                {
+                    reporter.put("id", "557058:7d269933-7525-46a1-8fea-a35b6caa1205");
+                }
+
+                ObjectNode priority = fields.putObject("priority");
+                {
+                    priority.put("name", "Should");
+                }
+
+                ObjectNode assignee = fields.putObject("assignee");
+                {
+                    assignee.put("displayName", "Akshay Rokade");
+                }
+            }
+        }
+
+// Connect Jackson ObjectMapper to Unirest
+        Unirest.setObjectMapper(new ObjectMapper() {
+            private com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper
+                    = new com.fasterxml.jackson.databind.ObjectMapper();
+
+            public <T> T readValue(String value, Class<T> valueType) {
+                try {
+                    return jacksonObjectMapper.readValue(value, valueType);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public String writeValue(Object value) {
+                try {
+                    return jacksonObjectMapper.writeValueAsString(value);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+// This code sample uses the  'Unirest' library:
+// http://unirest.io/java.html
+        HttpResponse<JsonNode> response = Unirest.post(jiraUrl + "/rest/api/3/issue")
+                .basicAuth(userId, token)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .body(payload)
+                .asJson();
+
+        System.out.println(response.getBody());
     }
 
     private GetRequest getConnection() {
